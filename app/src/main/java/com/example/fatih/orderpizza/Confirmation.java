100a84e185202f75d57734c4029110857f325a67
Fatih <mfatihinan@gmail.com>
2017-10-04 16:58:24 -0400

    connection to confirmation screen is done but data does not come correctly

--- app/src/main/java/com/example/fatih/orderpizza/Confirmation.java
+++ app/src/main/java/com/example/fatih/orderpizza/Confirmation.java
@@ -30,18 +30,54 @@ Customer Information
 - Card Number   : cardnumber
 - Card Type     : cardtype
 - Expiry Date   : expirydate
-
-
-
+*/
 
         Intent isim = getIntent();
 
-        TextView text01 = (TextView) findViewById(R.id.FullName22);
-        String vFullName = isim.getExtras().getString("FullName2");
-        text21.setText(vFullName);
+        TextView text01 = (TextView) findViewById(R.id.pizzaTypeData);
+        String sPizzaType = isim.getExtras().getString("pizzatype");
+        text01.setText(sPizzaType);
+
+        TextView text02 = (TextView) findViewById(R.id.pizzaSizeData);
+        String sPizzaSize = isim.getExtras().getString("pizzasize");
+        text02.setText(sPizzaSize);
+
+        TextView text03 = (TextView) findViewById(R.id.toppingsData);
+        String sToppings = isim.getExtras().getString("toppings");
+        text03.setText(sToppings);
+
+        TextView text04 = (TextView) findViewById(R.id.pizzaSizeData);
+        String sName = isim.getExtras().getString("name");
+        text04.setText(sName);
+
+        TextView text05 = (TextView) findViewById(R.id.pizzaSizeData);
+        String sPostalCode = isim.getExtras().getString("postalcode");
+        text05.setText(sPostalCode);
+
+        TextView text06 = (TextView) findViewById(R.id.pizzaSizeData);
+        String sTelephone = isim.getExtras().getString("telephone");
+        text06.setText(sTelephone);
+
+        TextView text07 = (TextView) findViewById(R.id.pizzaSizeData);
+        String sPaymentMethod = isim.getExtras().getString("paymentmethod");
+        text07.setText(sPaymentMethod);
+
+        TextView text08 = (TextView) findViewById(R.id.pizzaSizeData);
+        String sCardNumber = isim.getExtras().getString("cardnumber");
+        text08.setText(sCardNumber);
+
+        TextView text09 = (TextView) findViewById(R.id.pizzaSizeData);
+        String sCardType = isim.getExtras().getString("cardtype");
+        text09.setText(sCardType);
+
+        TextView text10 = (TextView) findViewById(R.id.pizzaSizeData);
+        String sExpiryDate = isim.getExtras().getString("expirydate");
+        text10.setText(sExpiryDate);
 
 
-*/
+
+
+
 
     }
 }
