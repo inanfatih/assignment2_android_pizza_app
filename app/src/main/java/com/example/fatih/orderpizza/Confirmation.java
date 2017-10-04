2667ad1b10fab854b7e74dd0fb68073f38ec9f9b
Fatih <mfatihinan@gmail.com>
2017-10-04 16:24:31 -0400

    confirmation screen designed

--- app/src/main/java/com/example/fatih/orderpizza/Confirmation.java
+++ app/src/main/java/com/example/fatih/orderpizza/Confirmation.java
@@ -1,7 +1,14 @@
 package com.example.fatih.orderpizza;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static com.example.fatih.orderpizza.R.id.meatsupreme;
 
 public class Confirmation extends AppCompatActivity {
 
@@ -9,5 +16,32 @@ public class Confirmation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation);
+/*
+Data to be shown:
+  info       : name in the intent
+- Pizza Type : pizzatype
+- Pizza Size : pizzasize
+- Toppings:    toppings
+Customer Information
+- Name          : name
+- Address       : postalcode
+- Telephone     : telephone
+- Payment Method: paymentmethod
+- Card Number   : cardnumber
+- Card Type     : cardtype
+- Expiry Date   : expirydate
+
+
+
+
+        Intent isim = getIntent();
+
+        TextView text21 = (TextView) findViewById(R.id.FullName22);
+        String vFullName = isim.getExtras().getString("FullName2");
+        text21.setText(vFullName);
+ */
+
+
+
     }
 }
